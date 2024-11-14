bbe277c79961b2fe5c5ff7784344d3ae1053cbd0
ArthurSZANTYR <szantyrarthur.pto@gmail.com>
2024-11-14 14:42:30 +0100

    led mode + imu read ok

--- Proto-1j/src/src.ino
+++ Proto-1j/src/src.ino
@@ -1,4 +1,6 @@
 #include "./led/led.h"
+#include "FastIMU.h"
+#include <Wire.h>
 
 // Définir les constantes
 #define NUM_LEDS 9
@@ -15,11 +17,33 @@ long intervalLEDs;     // Intervalle pour l'animation des LEDs
 // Objets
 LEDController ledController(LED_PIN, NUM_LEDS);
 
+// Configuration IMU
+#define IMU_ADDRESS 0x68
+MPU6050 IMU;
+calData calib = {0};
+AccelData accelData;
+GyroData gyroData;
+MagData magData;
+
 // Variables pour gérer le temps
 unsigned long previousMillisLEDs = 0;
 
+unsigned long previousMillisIMU = 0;
+const long imuInterval = 50; // Fréquence de lecture IMU
+
 void setup() {
     Serial.begin(115200);
+
+    Wire.begin(1, 2);
+    Wire.setClock(400000);
+
+    // Initialisation IMU
+    int err = IMU.init(calib, IMU_ADDRESS);
+    if (err != 0) {
+        Serial.print("Erreur d'initialisation IMU: ");
+        Serial.println(err);
+        while (true) {}
+    }
 
     if (mode == 1) {
         intervalLEDs = 100;
@@ -44,6 +68,29 @@ void loop() {
       if (mode == 2) {
           ledController.update_longrun();
       }
+    }
+
+    // Lecture IMU sans delay
+    if (currentMillis - previousMillisIMU >= imuInterval) {
+        previousMillisIMU = currentMillis;
+
+        IMU.update();
+        IMU.getAccel(&accelData);
+        Serial.print(accelData.accelX); Serial.print("\t");
+        Serial.print(accelData.accelY); Serial.print("\t");
+        Serial.println(accelData.accelZ);
+
+        IMU.getGyro(&gyroData);
+        Serial.print(gyroData.gyroX); Serial.print("\t");
+        Serial.print(gyroData.gyroY); Serial.print("\t");
+        Serial.println(gyroData.gyroZ);
+
+        if (IMU.hasMagnetometer()) {
+            IMU.getMag(&magData);
+            Serial.print(magData.magX); Serial.print("\t");
+            Serial.print(magData.magY); Serial.print("\t");
+            Serial.println(magData.magZ);
+        }
     }
 
 }
